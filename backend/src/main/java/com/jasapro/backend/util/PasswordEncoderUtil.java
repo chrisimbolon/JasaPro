7e66cabd5e1608a17bf5496e93c982b54af1ef79
Christyan Simbolon <sambungdata@gmail.com>
2025-12-17 00:13:47 +0700

    feat: Create PasswordEncoderUtil configuration class

---
+++ backend/src/main/java/com/jasapro/backend/util/PasswordEncoderUtil.java
@@ -0,0 +1,15 @@
+package com.jasapro.backend.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderUtil {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
