c796fea69933ae44e43bf840fb7bb89716f9e22e
Christyan Simbolon <sambungdata@gmail.com>
2025-12-27 22:00:01 +0700

    feat(provider): add provider repository

---
+++ backend/src/main/java/com/jasapro/backend/modules/provider/ProviderRepository.java
@@ -0,0 +1,14 @@
+package com.jasapro.backend.modules.provider;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProviderRepository extends JpaRepository<Provider, Long> {
+
+    Optional<Provider> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
